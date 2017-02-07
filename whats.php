d144ca106de8f62418c540ba756cae2e4710add2
Santiago Bernal B <exactlimon@gmail.com>
2017-02-07 12:48:18 -0500

    código
    código

---
+++ whats.php
@@ -0,0 +1,9 @@
+<h2>Ver estados de WhatsApp</h2>
+<form action="https://sro.whatsapp.net/client/iphone/iq.php" method="post">
+
+<input name="cd" type="hidden" value="1">
+<input name="cc" type="hidden" value="57">
+<input name="me" type="hidden" value="000000"> 
+Ingresa numero de telefono: <input name="u[]" type="text" value="">
+<input type="submit" value="ver estado">
+</form>
